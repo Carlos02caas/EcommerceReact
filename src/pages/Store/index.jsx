e7bb8427185dbeb098b8d52c595ad0e1b66818f7
Carlos Andres Alavez <carlos@ticsup.com>
2023-06-28 17:22:58 -0500

    Consumo API platzi, agregando información a las cards

--- src/pages/Store/index.jsx
+++ src/pages/Store/index.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from '../../components/Layout';
 import { Card } from '../../components/Card';
 
 const Store = () => {
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data));
+    },[])
+
     return (
         <Layout>
             Store Section
-            <Card/>
+            <div className=" grid gap-12 grid-cols-4 w-full max-w-screen-lg">
+                {
+                    items?.map( item =>(
+                        <Card key={item.id} data={item}/>
+                    ))
+                }
+            </div>
+            
+            
         </Layout>
     );
 }
